eb86b090cbee215464b32aefa62cf19ff90d169a
Yiannis Yiakoumis <yiannisy@stanford.edu>
2014-01-25 17:03:37 -0800

    display/fitler changes.

--- logs/admin.py
+++ logs/admin.py
@@ -27,14 +27,19 @@ class TransferLogAdmin(admin.ModelAdmin):
     list_display = ('location','timestamp','client','in_pkts','out_pkts','in_bytes','out_bytes')
     list_filter = ['client']
 
+class BandwidthLogAdmin(admin.ModelAdmin):
+    list_display = ('location','timestamp','client','in_bytes','out_bytes','in_avgrate_bps','out_avgrate_bps')
+    list_filter = ['client']
+    
+
 class ClientAdmin(admin.ModelAdmin):
-    list_display = ('ip_address', 'mac_address', 'type', 'os', 'user', 'bands_5GHz', 
+    list_display = ('ip_address', 'mac_address', 'location','type', 'os', 'user', 'bands_5GHz', 
                     'last_seen','last_heard','last_detected_5GHz','last_detected','last_detected_2GHz',
                     'bytes_dl_last','bytes_upl_last','netflix_mins','youtube_mins','bytes_dl','bytes_upl',
                     'rtt_samples','pkts_dl','pkts_upl',)
     list_filter = ('type','bands_5GHz','last_detected','last_seen','last_heard','user',
-                   'last_detected_5GHz','last_detected_2GHz')
-    search_fields = ('ip_address','type','os','user')
+                   'last_detected_5GHz','last_detected_2GHz','location')
+    search_fields = ('ip_address','type','os','user','location','mac_address')
     actions = ['show_stats']
 
     def show_stats(self, request, queryset):
@@ -50,4 +55,5 @@ admin.site.register(RttLog, RttLogAdmin)
 admin.site.register(NetflixLog, NetflixLogAdmin)
 admin.site.register(YoutubeLog, YoutubeLogAdmin)
 admin.site.register(TransferLog, TransferLogAdmin)
+admin.site.register(BandwidthLog, BandwidthLogAdmin)
 admin.site.register(Client, ClientAdmin)
